06d38061eee7da9d1da5b9bbdb8d4f162e41e093
Ricardo Valdovinos <RicardoValdovinos1996@gmail.com>
2020-05-10 23:36:12 -0700

    Cleared some TODO's

--- src/main/java/modals/PreviewModal.java
+++ src/main/java/modals/PreviewModal.java
@@ -1,22 +1,16 @@
 package modals;
 
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import screenshots.Screenshot;
-
-import java.io.File;
 
 public class PreviewModal {
     private static final Stage stage = new Stage();
     private static final int CONTENT_WIDTH = 750;
     private static final int CONTENT_HEIGHT = 800;
 
-    // TODO: find a better way to do this. Shouldn't be creating the same objects each call.
-    // TODO: find a way to close on lose focus while still allowing resize.
-    // TODO: REFACTOR! REFACTOR! REFACTOR!
     public static void showPreview(Screenshot screenshot) {
         if (stage.isShowing()) {
             stage.hide();
