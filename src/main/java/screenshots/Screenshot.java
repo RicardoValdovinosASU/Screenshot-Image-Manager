0b186f8d9e64567bdab9a49bfdba327ecd7c9928
Ricardo Valdovinos <RicardoValdovinos1996@gmail.com>
2020-05-12 18:30:18 -0700

    Added BasicFileAttributes info

--- src/main/java/screenshots/Screenshot.java
+++ src/main/java/screenshots/Screenshot.java
@@ -2,9 +2,12 @@ package screenshots;
 
 import javafx.scene.image.Image;
 
+import java.nio.file.attribute.BasicFileAttributes;
+
 public class Screenshot extends Image {
     private String location;
     private String name;
+    private BasicFileAttributes basicFileAttributes;
 
     public Screenshot(String location) {
         super(location);
@@ -27,5 +30,17 @@ public class Screenshot extends Image {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getDateCreated() {
+        return basicFileAttributes.creationTime().toString();
+    }
+
+    public String getLastModified(){
+        return basicFileAttributes.lastModifiedTime().toString();
+    }
+
+    public void setBasicFileAttributes(BasicFileAttributes basicFileAttributes) {
+        this.basicFileAttributes = basicFileAttributes;
     }
 }
